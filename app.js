988d68f62c3f5d02ce2e815dfab1afff204c94d3
CHUCHUDAN <daniel870217@gmail.com>
2023-02-03 15:52:11 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const exphbs = require('express-handlebars')
 //引入method-override
 const methodOverride = require('method-override')
 
-//引入mongoose
-const mongoose = require('mongoose')
 
 //引入router
 const routes = require('./routes')
@@ -21,24 +19,8 @@ const routes = require('./routes')
 //引入body-parser
 const bodyParser = require('body-parser')
 
-//如果是非正式環境，引入dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-//運用mongoose跟mongodb連線
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-//取得資料連線狀態
-const db = mongoose.connection
-//連線異常
-db.on('error', () => {
-  console.log('Mongodb error!')
-})
-//連線成功
-db.once('open', () => {
-  console.log('Mongodb connected!')
-})
+//引入mongoose模組
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/RestSeeder.js
+++ models/seeds/RestSeeder.js
@@ -1,29 +1,14 @@
-//引入mongoose
-const mongoose = require('mongoose')
-
 //引入rest model
 const Rest = require('../rest')
+
+//引入mongoose模組
+const db = require('../../config/mongoose')
 
 //引入json檔案
 const restaurantList = require('../../restaurant.json')
 
-//如果是非正式環境，引入dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-//運用mongoose跟mongodb連線
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-//取得資料連線狀態
-const db = mongoose.connection
-//連線異常
-db.on('error', () => {
-  console.log('Mongodb error!')
-})
 //連線成功
 db.once('open', () => {
-  console.log('Mongodb connected!')
   restaurantList.results.forEach(element => {
     Rest.create({
       id: `${element.id}`,
