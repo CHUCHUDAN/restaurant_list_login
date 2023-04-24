1e8236bf17dbfdbd28590687bda766a080e0af2f
CHUCHUDAN <daniel870217@gmail.com>
2023-04-24 14:13:16 +0800

    updated RestSeeder.js

--- models/seeds/RestSeeder.js
+++ models/seeds/RestSeeder.js
@@ -28,36 +28,37 @@ const SEED_USER = [
 //引入json檔案
 const restaurantList = require('../../restaurant.json')
 
-db.once('open', async () => {
-  try {
-    await Promise.all(
-      SEED_USER.map(async (user, user_index) => {
-        try {
-          const salt = await bcrypt.genSalt(10)
-          const hash = await bcrypt.hash(user.password, salt)
-          const createdUser = await User.create({
-            name: user.name,
-            email: user.email,
-            password: hash
-          })
-          console.log('user created')
-          const dataList = restaurantList.results
-          const userRestaurant = []
-          dataList.forEach((restaurant, rest_index) => {
-            if (rest_index >= 3 * user_index && rest_index < 3 * (user_index + 1)) {
-              restaurant.userId = createdUser._id
-              userRestaurant.push(restaurant)
-            }
-          })
-          await Rest.create(userRestaurant)
-        } catch (error) {
-          console.error(error)
-        }
-      })
-    )
+db.once('open', () => {
+  Promise.all(
+    SEED_USER.map(async (user, user_index) => {
+      try {
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(user.password, salt)
+        const createdUser = await User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
+        })
+        console.log('user created!')
+        const dataList = restaurantList.results
+        const userRestaurant = []
+        dataList.forEach((restaurant, rest_index) => {
+          if (rest_index >= 3 * user_index && rest_index < 3 * (user_index + 1)) {
+            restaurant.userId = createdUser._id
+            userRestaurant.push(restaurant)
+          }
+        })
+        await Rest.create(userRestaurant)
+        console.log('restaurant created!')
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  ).then(() => {
     console.log('done')
     process.exit()
-  } catch (error) {
-    console.error(error)
-  }
-})
+  }).catch(error => {
+    console.log(error)
+  })
+}
+)
